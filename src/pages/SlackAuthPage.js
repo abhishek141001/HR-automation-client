70e8293948ad83c0ec8870c7fcfde7e50accdc37
abhishek <ojhaabhishekraj14@gmail.com>
2025-01-18 14:53:52 +0530

    updated slackauth url

--- src/pages/SlackAuthPage.js
+++ src/pages/SlackAuthPage.js
@@ -3,16 +3,16 @@ import {Button} from '@mui/material'
 
 export default function SlackAuthPage() {
 
-  const handleSlackAUth = async() =>{
-    const authRespone = await fetch('https://api.bulkbuddy.site/slack/oauth/start')
-    const authData = await authRespone.json()
-    console.log(authData)
-  }
+  // const handleSlackAUth = async() =>{
+  //   const authRespone = await fetch('https://api.bulkbuddy.site/slack/oauth/start')
+  //   const authData = await authRespone.json()
+  //   console.log(authData)
+  // }
 
 
   return (
     <div className='w-full h-screen flex justify-center items-center'> 
-        <h3 className='text-4xl'>Authinticate with <Button variant='text' onClick={handleSlackAUth}>Slack</Button> to create automation</h3>
+        <h3 className='text-4xl'>Authinticate with <a href='https://api.bulkbuddy.site/slack/oauth/start'>Slack</a> to create automation</h3>
     </div>
   )
 }
